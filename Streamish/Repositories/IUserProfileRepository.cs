7105b5afd994f1dfe6309e8218507f85e4034088
Logan Demmy <frank.demmy@gmail.com>
2021-07-07 15:35:58 -0500

    Crud for user

---
+++ Streamish/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,19 @@
+﻿using Streamish.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Streamish.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        public List<UserProfile> GetAll();
+        public UserProfile GetById(int id);
+        public void Add(UserProfile userProfile);
+        public void Update(UserProfile userProfile);
+        public void Delete(int id);
+
+       
+    }
+}
---
+++ Streamish/Repositories/UserProfileRepository.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+using Streamish.Utils;
+using Streamish.Models;
+using Streamish.Repositories;
+
+namespace Streamish.Repositories
+{
+    public class UserProfileRepository : BaseRepository, IUserProfileRepository
+    {
+        public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
+
+
+
+        public List<UserProfile> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = @"
+                                    Select Id, Name, Email, Bio, ImageUrl, DateCreated
+                                    from UserProfile";
+
+                    var reader = cmd.ExecuteReader();
+                    var users = new List<UserProfile>();
+                    while (reader.Read())
+                    {
+                        users.Add(new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            Name = DbUtils.GetString(reader, "Name"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
+                            ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
+                           
+                        });
+                    }
+
+                    reader.Close();
+
+                    return users;
+                }
+            }
+        }
+        public UserProfile GetById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                    Select u.[Name], u.Email, u.Bio, u.ImageUrl, u.DateCreated,
+                                    p.id as VideoId, P.Title, P.Caption, P.DateCreated as VideoDateCreated, P.ImageUrl as VideoURL,
+                                    c.Id AS CommentId, c.Message, c.UserProfileId AS CommentUserProfileId
+                                    from UserProfile u left join Video p on u.Id = p.UserProfileId
+                                    left join Comment c on c.VideoId = p.id
+                                    where u.Id = @Id";
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    var reader = cmd.ExecuteReader();
+                    UserProfile userProfile = null;
+                    while (reader.Read())
+                    {
+                        if (userProfile == null)
+                        {
+                            userProfile = new UserProfile()
+                            {
+                                Id = id,
+                                Name = DbUtils.GetString(reader, "Name"),
+                                Email = DbUtils.GetString(reader, "Email"),
+                                DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
+                                ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
+                                
+                                Videos = new List<Video>()
+                            };
+                        }
+
+                        if (DbUtils.IsNotDbNull(reader, "VideoId"))
+                        {
+                            Video post = null;
+                            if (post == null)
+                            {
+                                post = new Video()
+                                {
+                                    Id = DbUtils.GetInt(reader, "VideoId"),
+                                    Title = DbUtils.GetString(reader, "Title"),
+                                    Caption = DbUtils.GetString(reader, "Caption"),
+                                    DateCreated = DbUtils.GetDateTime(reader, "VideoDateCreated"),
+                                    ImageUrl = DbUtils.GetString(reader, "VideoURL"),
+                                    Comments = new List<Comment>()
+                                };
+                            }
+                            if (DbUtils.IsNotDbNull(reader, "CommentId"))
+                            {
+                                post.Comments.Add(new Comment()
+                                {
+                                    Id = DbUtils.GetInt(reader, "CommentId"),
+                                    Message = DbUtils.GetString(reader, "Message"),
+                                    VideoId = id,
+                                    UserProfileId = DbUtils.GetInt(reader, "CommentUserProfileId"),
+
+                                });
+                            }
+                            userProfile.Videos.Add(post);
+                        };
+
+
+                    }
+                    reader.Close();
+
+                    return userProfile;
+                }
+            }
+        }
+        public void Add(UserProfile userProfile)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"Insert into UserProfile ([Name], Email, DateCreated, ImageUrl, Bio) 
+                                        output inserted.Id
+                                        values (@name, @Email, @DateCreated, @ImageUrl, @Bio);";
+
+                    DbUtils.AddParameter(cmd, "@name", userProfile.Name);
+                    DbUtils.AddParameter(cmd, "@Email", userProfile.Email);
+                    DbUtils.AddParameter(cmd, "@DateCreated", userProfile.DateCreated);
+                    DbUtils.AddParameter(cmd, "@ImageUrl", userProfile.ImageUrl);
+                   
+                    userProfile.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void Update(UserProfile userProfile)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE UserProfile
+                           SET [Name] = @Name,
+                               Email = @Email,
+                               DateCreated = @DateCreated,
+                               ImageUrl = @ImageUrl,
+                               Bio = @Bio
+                         WHERE Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@Name", userProfile.Name);
+                    DbUtils.AddParameter(cmd, "@Email", userProfile.Email);
+                    DbUtils.AddParameter(cmd, "@DateCreated", userProfile.DateCreated);
+                    DbUtils.AddParameter(cmd, "@ImageUrl", userProfile.ImageUrl);
+                    DbUtils.AddParameter(cmd, "@Id", userProfile.Id);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "Delete from UserProfile where id = @id; ";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+    }
+}
